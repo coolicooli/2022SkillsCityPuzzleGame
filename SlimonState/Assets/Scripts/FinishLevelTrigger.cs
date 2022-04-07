36d385f37f0e9cfd70f8a90e08de8355df31c637
mufcberry <reagan.berry2@gmail.com>
2022-04-07 13:38:23 +0100

    Added Annotation for finish rigger script

--- SlimonState/Assets/Scripts/FinishLevelTrigger.cs
+++ SlimonState/Assets/Scripts/FinishLevelTrigger.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FinishLevelTrigger : MonoBehaviour
 {
     public AudioSource playCSound;
-
+    // Soon as player hits the finish trigger the finish sound plays
     private void OnTriggerEnter(Collider other) 
     {
         playCSound.Play();
