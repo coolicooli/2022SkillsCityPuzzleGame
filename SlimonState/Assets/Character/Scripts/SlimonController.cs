51266f51b42052f07e0d68b81dee33abcc15fac5
Cameron <cameronjamesbrown21@gmail.com>
2022-04-08 11:09:51 +0100

    CB: Few bug fixes

--- SlimonState/Assets/Character/Scripts/SlimonController.cs
+++ SlimonState/Assets/Character/Scripts/SlimonController.cs
@@ -21,12 +21,9 @@ public class SlimonController : MonoBehaviour
     private GameObject playerSprite;
     public AudioSource bumbSound;
     public LayerMask solidObjectsLayer;
-    public GameObject box;
     float delay = 0.15f;
     float remainingDelay;
     bool m_Started;
-    float worldWidth;
-    float worldHeight;
     Vector3 positionChange; 
     MoveDirection playerFacing = MoveDirection.Down;
     MoveDirection prevPlayerFacing;
@@ -37,12 +34,8 @@ public class SlimonController : MonoBehaviour
     {
         animator = playerSprite.GetComponent<Animator>();
         m_Started = true;
-        worldWidth = box.GetComponent<MeshRenderer>().bounds.size.x;
-        worldHeight = box.GetComponent<MeshRenderer>().bounds.size.y;
-        Debug.Log(worldWidth + "= width,  " + worldHeight + " = height");
         mainCam = Camera.main;
         spriteTransform = playerSprite.GetComponent<Transform>();
-        Debug.Log(spriteTransform);
     }
 
     // Update is called once per frame
@@ -63,8 +56,6 @@ public class SlimonController : MonoBehaviour
             {
                 prevPlayerFacing = playerFacing;
                 SetFaceingDirection();
-                Debug.Log(prevPlayerFacing+ "   previous");
-                Debug.Log(playerFacing + "   current");
                 animator.SetFloat("moveX", input.x);
                 animator.SetFloat("moveY", input.y);
                 if (playerFacing != prevPlayerFacing)
@@ -81,48 +72,76 @@ public class SlimonController : MonoBehaviour
                 animator.SetFloat("moveY", input.y);
                 var targetPos = transform.position;
                 positionChange = Vector3.zero;
-                positionChange.x -= input.x;
-                positionChange.y += input.y;
+                positionChange.x += input.x;
+                positionChange.z += input.y;
                 var test = targetPos + positionChange;
-                if (isWalkerble(test))
+                if (isWalkable(test))
                 {
- 
+                    Debug.Log("Walkin");
                     targetPos += positionChange;
-
-                    
                     StartCoroutine(Move(targetPos));
                 }
 
             }
-            //animator.SetBool("isRunning", isRunning);
             animator.SetBool("isMoving", isMoving);
         }
     }
 
     void LateUpdate()
     {
-        
+        playerSprite.transform.LookAt(mainCam.transform);
     }
     IEnumerator Move(Vector3 targetPos)
     {
         isMoving = true;
-        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        while ((targetPos - transform.position).sqrMagnitude > 0.000001f)
         {
             transform.position = Vector3.MoveTowards(transform.position, targetPos, walkSpeed * Time.deltaTime);
             yield return null;
         }
         transform.position = targetPos;
         isMoving = false;
+
+        yield return null;
     }
 
-    private bool isWalkerble(Vector3 targetPos)
+    private bool isWalkable(Vector3 targetPos)
     {
-        if (Physics2D.OverlapCircle(targetPos, 0.3f, solidObjectsLayer) != null)
+        Collider[] hitCollider = Physics.OverlapBox(targetPos, new Vector3(0.3f, 0.3f, 0.3f), Quaternion.identity, solidObjectsLayer);
+        if (hitCollider.Length > 0)
         {
             bumbSound.Play();
             return false;
         }
         return true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if (m_Started)
+        {
+            Vector3 test = transform.position;
+            if (playerFacing == MoveDirection.Up)
+            {
+                test.z += 1;
+            }
+            else if (playerFacing == MoveDirection.Down)
+            {
+                test.z -= 1;
+            }
+            else if (playerFacing == MoveDirection.Left)
+            {
+                test.x -= 1;
+            }
+            else if (playerFacing == MoveDirection.Right)
+            {
+                test.x += 1;
+            }
+
+            
+            Gizmos.DrawWireCube(test, new Vector3(0.3f, 0.3f, 0.3f));
+        }
     }
 
     void SetFaceingDirection()
